ab9142d0ba0c7a1fab25f177a4a8c96ecd0eae69
Charitha-Reddy1 <carg2800@gmail.com>
2026-03-03 15:30:04 +0530

    commit

--- Part3/page28a.js
+++ Part3/page28a.js
@@ -3,18 +3,33 @@ const products=[
     {id:2,name:'Product 2',price:160},
     {id:3,name:'Product 3',price:135}
 ]
-//display all products using forEach
 
+let cart=[]
+
+//display all products using forEach
+function dispro(){
 products.forEach((prod)=>{
     console.log(prod)
 })
+}
 
 //display products greater with price above 136
-
+function prodfilter(){
 console.log(products.filter((prod)=>prod.price>136))
-
+}
 //add product 2 to cart using spread operator with a
-//new key quantity and assign 1
+function addToCart(id){
+    let item={...products[id-1],rating:1}
+    cart.push(item)
+}
 
-let cart={...products[1],rating:1}
+//new key quantity and assign 1
+function disCart(){
 console.log(cart)
+}
+
+dispro()
+prodfilter()
+addToCart(2)
+disCart()
+
